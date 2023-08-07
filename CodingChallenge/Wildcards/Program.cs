9bafe55675be74fd7bb9bc457b3084c024f04147
Pai <pai.nuneza@gmail.com>
2023-08-08 01:55:04 +0800

    Added WildCards - Initial implementation

---
+++ CodingChallenge/Wildcards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+bool Wildcards(string str)
+{
+    // Split the input string into the pattern and the target string
+    string[] parts = str.Split(' ');
+    string pattern = parts[0];
+    string target = parts[1];
+
+    // Replace the optional {N} with the appropriate regex pattern
+    pattern = Regex.Replace(pattern, @"\{(\d+)\}", "{$1}");
+
+    // Escape special characters that have meanings in regex
+    pattern = Regex.Escape(pattern);
+
+    // Replace + with a single alphabet character
+    pattern = pattern.Replace(@"\+", "[a-zA-Z]");
+
+    // Replace $ with a number 1-9
+    pattern = pattern.Replace(@"\$", "[1-9]");
+
+    // Replace * with the same character repeated three times
+    pattern = pattern.Replace(@"\*", "(.)\\1{2}");
+
+    // Check if the target string matches the pattern
+    return Regex.IsMatch(target, pattern);
+}
+
+Console.WriteLine(Wildcards("+++++* abcdehhhhhh")); // Output: False
+Console.WriteLine(Wildcards("$**+*{2} 9mmmrrrkbb")); // Output: True
